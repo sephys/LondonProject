ee260c5f421a139fd861f76680deecaedff440dd
j00ke <m.d4rin@gmail.com>
2014-03-18 09:11:14 +0100

    1C

---
+++ Frame.java
@@ -0,0 +1,43 @@
+package london;
+import java.awt.*;
+import javax.swing.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joke
+ * Représente la fenêtre principale du jeu
+ */
+public class Frame extends JFrame{
+    
+    
+    public Frame() {
+        this.setTitle("London");
+        this.setSize(1000,1000);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setVisible(true);
+    }
+    
+    public void start(){
+        this.setContentPane(new Plateau());
+    }
+    
+    public void setting(){
+        JPanel p=new JPanel(new BorderLayout());
+        this.add(p,BorderLayout.SOUTH);
+        JTextField jt=new JTextField("Nombre de joueurs : ");
+        JComboBox jc=new JComboBox();
+        jc.addItem("2 joueurs");
+        jc.addItem("3 joueurs");
+        jc.addItem("4 joueurs");
+    }
+            
+    
+    
+}
---
+++ Plateau.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package london;
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Joke
+ * Représente le panel contenant le plateau
+ */
+public class Plateau extends JPanel {
+    Image image;
+    
+    public Plateau(){
+        this.setLayout(new BorderLayout());
+        image = getToolkit().getImage("/Users/Joke/NetBeansProjects/London/src/london/plateau.png");
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+     super.paintComponent(g);
+     g.drawImage (image, 0, 0, getWidth(),getHeight(),this);
+     repaint();
+   }
+    
+    
+}
